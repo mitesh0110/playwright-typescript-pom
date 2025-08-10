028ca7f8ee12c5e440fca28563697b7d84a3c144
mitesh0110 <miteshbhatt0110@gmail.com>
2025-08-11 00:20:10 +1200

    rimraf changes

--- global-setup.ts
+++ global-setup.ts
@@ -1,10 +1,8 @@
-import rimraf from "rimraf";
+import { rimraf } from "rimraf";
 // This file is used to clean up the allure results directory before running tests
 // It ensures that previous test results do not interfere with the current test run
 
 async function globalSetup(): Promise<void> {
-    await new Promise(resolve => {
-        rimraf(`./allure-results`, resolve);
-    });
+    await rimraf("./allure-results");
 }
 export default globalSetup;
